41e973f5c7aa6a125c255a04b896fb26a03fbb99
thanhvule0310 <thanhvule0310@gmail.com>
2022-04-28 08:53:02 +0700

    docs: swagger integration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,10 +15,19 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('Documents for TodoAPI')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
   await app.listen(configService.get<number>('app.port'));
 
   const domain = configService.get<string>('app.domain');
 
+  Logger.log(`API documentations is running on ${domain}/${globalPrefix}/docs`);
   Logger.log(`Application is running on ${domain}/${globalPrefix}`);
 }
 
