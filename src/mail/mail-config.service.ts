797d3dd7dc16febe764966e3bd81ae1ef2b272d8
Vu Le Thanh <thanhvule0310@gmail.com>
2022-06-14 18:48:43 +0700

    fix(mail): oauth2 config

--- src/mail/mail-config.service.ts
+++ src/mail/mail-config.service.ts
@@ -3,10 +3,36 @@ import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as path from 'path';
+import { Auth, google } from 'googleapis';
 
 @Injectable()
 export class MailConfigService implements MailerOptionsFactory {
-  constructor(private configService: ConfigService) {}
+  private clientId: string;
+  private clientSecret: string;
+  private oauth2Client: Auth.OAuth2Client;
+  private accessToken: any;
+  private refreshToken: string;
+
+  constructor(private configService: ConfigService) {
+    this.clientId = this.configService.get<string>('mail.auth.clientId');
+    this.clientSecret = this.configService.get<string>(
+      'mail.auth.clientSecret',
+    );
+    this.oauth2Client = new google.auth.OAuth2(
+      this.clientId,
+      this.clientSecret,
+      this.configService.get<string>('mail.auth.redirectUri'),
+    );
+    this.refreshToken = this.configService.get<string>(
+      'mail.auth.refreshToken',
+    );
+    this.oauth2Client.setCredentials({
+      refresh_token: this.refreshToken,
+    });
+    this.accessToken = this.oauth2Client
+      .getAccessToken()
+      .then((token) => token);
+  }
 
   createMailerOptions(): MailerOptions | Promise<MailerOptions> {
     return {
@@ -14,12 +40,12 @@ export class MailConfigService implements MailerOptionsFactory {
         host: this.configService.get<string>('mail.host'),
         port: this.configService.get<string>('mail.port'),
         auth: {
-          type: 'OAuth2',
-          user: this.configService.get<string>('mail.mail'),
-          accessToken: this.configService.get<string>('mail.accessToken'),
-          clientId: this.configService.get<string>('mail.clientId'),
-          clientSecret: this.configService.get<string>('mail.secret'),
-          refreshToken: this.configService.get<string>('mail.refreshToken'),
+          type: this.configService.get<string>('mail.auth.type'),
+          user: this.configService.get<string>('mail.auth.user'),
+          accessToken: this.accessToken,
+          clientId: this.clientId,
+          clientSecret: this.clientSecret,
+          refreshToken: this.refreshToken,
         },
       },
       template: {
