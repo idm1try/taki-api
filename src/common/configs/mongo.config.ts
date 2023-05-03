f5c99c8d60029d2aa36f2ed98072282eb381e495
Vu Le Thanh <thanhvule0310@gmail.com>
2023-05-03 10:52:02 +0700

    chore: rename ENV mongodb

--- src/common/configs/mongo.config.ts
+++ src/common/configs/mongo.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("mongo", () => ({
-    uri: process.env.MONGO_URI,
+    uri: process.env.MONGO_URL,
     dbName: process.env.MONGO_DBNAME,
 }));
