0cfe903d2ab1bf8253384a07acca36a8a63e9dfd
Vu Le Thanh <thanhvule0310@gmail.com>
2023-05-08 11:23:22 +0700

    fix: only take nescessary fields in body request

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({
             skipMissingProperties: true,
+            whitelist: true,
         }),
     );
     app.useGlobalFilters(new AllExceptionsFilter());
